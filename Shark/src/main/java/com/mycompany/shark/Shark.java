0014955a92c93b986e2435df2d83105956e4abec
PhaiNawapon <phainawaponwork@gmail.com>
2024-08-26 00:04:42 +0700

    Add files via upload
    GUI

---
+++ Shark/src/main/java/com/mycompany/shark/Shark.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.shark;
+
+/**
+ *
+ * @author User
+ */
+public class Shark {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
